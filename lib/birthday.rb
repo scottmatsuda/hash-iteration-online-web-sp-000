a1fd91ca0e1241580be62886cc62c7208c844a39
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-31 17:17:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,5 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday
-  
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!" if age <= 12
+  end
 end
